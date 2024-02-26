fdb63a419b7c46ec941f61f8fe151ba72427da09
Harshan Attar <attarharshan411@gmail.com>
2024-02-26 23:40:25 +0530

    fixed the update cart feature

--- add-item.php
+++ add-item.php
@@ -37,25 +37,51 @@
     
     $db_select2 = mysqli_select_db($conn2, 'ecommerce') or die(mysqli_error($conn2));
     
-    $sql2 = "INSERT INTO cart SET 
+
+    // Check if the value already exists in Column A
+    $query = "SELECT * FROM cart WHERE all_items_id = '$item_id'";
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) > 0) {
+        // If the value exists, update Column B
+        $update_query = "UPDATE cart SET item_quantity = item_quantity+1 WHERE all_items_id = '$item_id'";
+        $res1 = mysqli_query($conn, $update_query);
+
+        if($res2==true)
+        {
+            $_SESSION['add'] = "Task Added Successfully.";
+            
+            header('location:'.'http://localhost/E-Commerce-Website/');
+            
+        }
+        else
+        {
+            $_SESSION['add_fail'] = "Failed to Add Task";
+            header('location:'.'http://localhost/E-Commerce-Website/'.'add-item.php');
+        }
+    } 
+    else {
+        // If the value does not exist, insert a new row
+        $sql2 = "INSERT INTO cart SET 
         all_items_id = '$all_items_id',
         item_name = '$item_name',
         item_price = '$item_price'
-    ";
+        ";
     
-    $res2 = mysqli_query($conn2, $sql2);
-    
-    if($res2==true)
-    {
-        $_SESSION['add'] = "Task Added Successfully.";
-        
-        header('location:'.'http://localhost/E-Commerce-Website/');
-        
-    }
-    else
-    {
-        $_SESSION['add_fail'] = "Failed to Add Task";
-        header('location:'.'http://localhost/E-Commerce-Website/'.'add-item.php');
+        $res2 = mysqli_query($conn2, $sql2);
+
+        if($res2==true)
+        {
+            $_SESSION['add'] = "Task Added Successfully.";
+            
+            header('location:'.'http://localhost/E-Commerce-Website/');
+            
+        }
+        else
+        {
+            $_SESSION['add_fail'] = "Failed to Add Task";
+            header('location:'.'http://localhost/E-Commerce-Website/'.'add-item.php');
+        }
     }
 
 ?>
