1aebe5d0d734cd5b7f408047cb59b7e953423210
Harshan-Attar <132804710+Harshan-Attar@users.noreply.github.com>
2024-02-28 20:11:57 +0530

    Revert "Merging banner with main"

--- add-item.php
+++ add-item.php
@@ -1,96 +1,87 @@
-<!-- <html>
-    <script>
-            
-            function update_subtotal(quantity) {
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "cart.php", true);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    console.log(xhr.responseText); // Output: Response from add-item.php
-                }
-            };
-            xhr.send("quantity=" + quantity);
+<?php 
+
+    // include('config/constants.php');
+    
+    if(isset($_POST['item_id']))
+    {
+        $item_id = $_POST['item_id'];
+        
+        $conn = mysqli_connect('localhost', 'root', 'root') or die(mysqli_error($conn));
+        
+        $db_select = mysqli_select_db($conn, 'ecommerce') or die(mysqli_error($conn));
+        
+        $sql = "SELECT * FROM all_items WHERE item_id=$item_id";
+        
+        $res = mysqli_query($conn, $sql);
+        
+        if($res==true)
+        {
+            while($row=mysqli_fetch_assoc($res))
+            {
+                $all_items_id = $row['item_id'];
+                $item_name = $row['item_name'];
+                $item_price = $row['item_price'];
+            }
         }
-    </script>
-</html> -->
-<?php
-
-include('config/constants.php');
-if (isset($_POST['item_id'])) {
-    $item_id = $_POST['item_id'];
-
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($conn));
-
-    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error($conn));
-
-    $sql = "SELECT * FROM all_items WHERE item_id=$item_id";
-
-    $res = mysqli_query($conn, $sql);
-
-    if ($res == true) {
-        while ($row = mysqli_fetch_assoc($res)) {
-            $all_items_id = $row['item_id'];
-            $item_name = $row['item_name'];
-            $item_price = $row['item_price'];
-        }
+        
+    }
+    else
+    {
+        header('location:'.'http://localhost/E-Commerce-Website/');
     }
 
-    $conn2 = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error($conn2));
 
-    $db_select2 = mysqli_select_db($conn2, DB_NAME) or die(mysqli_error($conn2));
 
+    
+    $conn2 = mysqli_connect('localhost', 'root', 'root') or die(mysqli_error($conn2));
+    
+    $db_select2 = mysqli_select_db($conn2, 'ecommerce') or die(mysqli_error($conn2));
+    
 
     // Check if the value already exists in all_items_id col
     $query = "SELECT * FROM cart WHERE all_items_id = '$item_id'";
     $result = mysqli_query($conn, $query);
 
-    if (mysqli_num_rows($result) > 0) {
+    if(mysqli_num_rows($result) > 0) {
         // If the value exists, update item_quantity col
         $update_query = "UPDATE cart SET item_quantity = item_quantity+1 WHERE all_items_id = '$item_id'";
         $res1 = mysqli_query($conn, $update_query);
-    } else {
+
+        // if($res2==true)
+        // {
+        //     $_SESSION['add'] = "Task Added Successfully.";
+            
+        //     header('location:'.'http://localhost/E-Commerce-Website/');
+            
+        // }
+        // else
+        // {
+        //     $_SESSION['add_fail'] = "Failed to Add Task";
+        //     header('location:'.'http://localhost/E-Commerce-Website/'.'add-item.php');
+        // }
+    } 
+    else {
         // If the value does not exist, insert a new row
         $sql2 = "INSERT INTO cart SET 
         all_items_id = '$all_items_id',
         item_name = '$item_name',
         item_price = '$item_price'
         ";
-
+    
         $res2 = mysqli_query($conn2, $sql2);
+
+        // if($res2==true)
+        // {
+        //     $_SESSION['add'] = "Task Added Successfully.";
+            
+        //     header('location:'.'http://localhost/E-Commerce-Website/');
+            
+        // }
+        // else
+        // {
+        //     $_SESSION['add_fail'] = "Failed to Add Task";
+        //     header('location:'.'http://localhost/E-Commerce-Website/'.'add-item.php');
+        // }
     }
-}
 
-
-
-
-
-
-//check if quantity is updated in cart.php
-if (isset($_POST['quantity'])) {
-    $item_quantiti = $_POST['quantity'];
-    $conn2 = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die(mysqli_error($conn2));
-    $query2 = "UPDATE cart SET item_quantity = $item_quantiti WHERE all_items_id = '$item_id'";
-    $result2 = mysqli_query($conn2, $query2);
-}
-
-
-// Update subtotal of items
-// Calculate the total value of items in the cart
-if (isset($_GET['subtotal_item_id'])) {
-    $subtotal_item_id = $_GET['subtotal_item_id'];
-    $quantity = $_GET['quantity'];
-
-    $conn3 = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die(mysqli_error($conn3));
-    $sql3 = "SELECT ROUND(item_price * $quantity, 2) AS subtotal FROM cart WHERE item_id = '$subtotal_item_id'";
-    $res3 = mysqli_query($conn3, $sql3);
-
-    if (mysqli_num_rows($res3) > 0) {
-        $row = mysqli_fetch_assoc($res3);
-        $subtotalValue = $row["subtotal"];
-        echo $subtotalValue;
-    } else {
-        echo "0";
-    }
-}
 ?>
