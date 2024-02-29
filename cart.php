4ba0ad5b20ed108a89fcb22b4f41ca0acb6bd5eb
Yash0082 <yash26022003@gmail.com>
2024-02-29 17:05:37 +0530

    added footer

--- cart.php
+++ cart.php
@@ -191,6 +191,73 @@
 
         </div>
     </section>
+
+    <section id= "newsletter" class=" section-p1 section-m1">
+        <div class="newstext">
+            <h4>Sign Up for Newsletter</h4>
+            <p>Get E-mail update about  our latestt shop and <span>Special Offers.</span></p>
+        </div>
+
+        <div class="form">
+            <input type="text" placeholder = "Your E-mail address">
+            <button class="normal"> Sign Up</button>
+        </div>
+    </section>
+
+    <footer class="section-p1">
+        <div class="col">
+            <img class="logo" src="img/H_Y-removebg-preview.png" alt="logo" >
+            <h4>Contact</h4>
+            <p><strong>Address</strong>: galli no. 3 Kisiko bhi pucho ghar tak chhodte</p>
+            <p><strong>Phone</strong>:+92 54835 93832/(+91) 54324 99479</p>
+            <p><strong>Hours</strong>: 10:00 - 18:00, Mon - Saturday</p>
+            
+            <div class="follow">
+                <h4>Follow Us</h4>
+                <div class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-pinterest-p"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col">
+            <h4>About</h4>
+            <a href="#" > About Us</a>
+            <a href="#" > Delivery Information</a>
+            <a href="#" > Privacy Policy</a>
+            <a href="#" > Terms and Conditions</a>
+            <a href="#" > Contact Us</a>
+        </div>
+
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="#" > Sign in</a>
+            <a href="#" > View Cart</a>
+            <a href="#" > My Wishlist</a>
+            <a href="#" > Track My Order</a>
+            <a href="#" > Help</a>
+        </div>
+
+        <div class="col install">
+            <h4> Install App</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="img/install/gogl-logo.png" alt="google-play" width="248px">
+                <img src="img/install/apl-logo.png" alt="apple-store" width="248px">
+            </div>
+            <p>Secured Payment Gateways</p>
+            <img class="pg" src="img/install/pg1.png" alt="payment-gateways">
+        </div>
+        <div class="copyright">
+            <p>&copy; 2023, H&Y </p>
+        </div>
+    </footer>
+
     <script src="script.js"></script>
 
 </body>
